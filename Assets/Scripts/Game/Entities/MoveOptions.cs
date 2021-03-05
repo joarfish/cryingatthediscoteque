6ed74d6527e69daf61fab57cd53b588876b495a7
Jonas Klein <hello@jonasklein.dev>
2021-03-05 23:58:00 +0100

    Add Player and MovementOption Idicator Stuff.

---
+++ Assets/Scripts/Game/Entities/MoveOptions.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+namespace Game.Entities
+{
+    public class MoveOptions : MonoBehaviour
+    {
+        private Mesh _mesh;
+        private Material _material;
+        private float _timer = 0.0f;
+        private int _rhythmTimePropertyIndex;
+        private AnimationCurve _animationCurve;
+    
+        void Awake()
+        {
+            _mesh = new Mesh { name = "OptionsMesh" };
+            gameObject.AddComponent<MeshFilter>().sharedMesh = _mesh;
+        
+            _material = new Material(Shader.Find("Unlit/MovementOptions"));
+            gameObject.AddComponent<MeshRenderer>().material = _material;
+            
+            _rhythmTimePropertyIndex = Shader.PropertyToID("_RhythmTime");
+
+            _animationCurve = new AnimationCurve();
+            _animationCurve.AddKey(0.0f, 0.0f);
+            _animationCurve.AddKey(0.25f, 0.125f);
+            _animationCurve.AddKey(0.5f, 0.4f);
+            _animationCurve.AddKey(0.75f, 0.75f);
+            _animationCurve.AddKey(1.0f, 1.0f);
+
+            CreateMesh();
+        }
+
+        void Update()
+        {
+            _timer = (_timer + Time.deltaTime) % 1.0f;
+            _material.SetFloat(_rhythmTimePropertyIndex,_animationCurve.Evaluate(_timer / 1.0f));
+        }
+
+        private void CreateMesh()
+        {
+            var vertices = new Vector3[]
+            {
+                new Vector3(-0.333333f, 0.0f, 0.333333f), // 0
+                new Vector3(0.333333f, 0.0f, 0.333333f), // 1
+                new Vector3(-0.333333f, 0.0f, -0.333333f), // 2
+                new Vector3(0.333333f, 0.0f, -0.333333f), // 3
+                new Vector3(-0.333333f, 0.0f, 1.0f), // 4
+                new Vector3(0.333333f, 0.0f, 1.0f), // 5
+                new Vector3(1.0f, 0.0f, 0.333333f), // 6
+                new Vector3(1.0f, 0.0f, -0.333333f), // 7
+                new Vector3(0.333333f, 0.0f, -1.0f), // 8
+                new Vector3(-0.333333f, 0.0f, -1.0f), // 9
+                new Vector3(-1.0f, 0.0f, -0.333333f), // 10
+                new Vector3(-1.0f, 0.0f, 0.333333f) // 11
+            };
+
+
+            var indices = new[]
+            {
+                4, 1, 0,
+                4, 5, 1,
+                1, 6, 7,
+                1, 7, 3,
+                2, 8, 9,
+                2, 3, 8,
+                11, 0, 2,
+                11, 2, 10
+            };
+            _mesh.SetVertices(vertices);
+            _mesh.triangles = indices;
+        }
+    }
+}
---
+++ Assets/Shader/MovementOptions.shader
@@ -0,0 +1,56 @@
+﻿Shader "Unlit/MovementOptions"
+{
+    Properties
+    {
+        _RhythmTime ("Time [0.0-1.0]", Range(0.0, 1.0)) = 0.0
+        _Color ("Color", vector) = (1, 0, 0, 1) 
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct vertex_input
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct fragment_input
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            float _RhythmTime;
+
+            fragment_input vert (vertex_input v)
+            {
+                if (v.vertex.x == 1.0 || v.vertex.x == -1)
+                {
+                    v.vertex.x = lerp(0.333333, 1.0, _RhythmTime) * v.vertex.x;
+                } else if(v.vertex.z == 1.0 || v.vertex.z == -1.0)
+                {
+                    v.vertex.z = lerp(0.333333, 1.0, _RhythmTime) * v.vertex.z;
+                }
+                fragment_input o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            float4 _Color;
+
+            fixed4 frag (fragment_input i) : SV_Target
+            {
+                return _Color;
+            }
+            ENDCG
+        }
+    }
+}
